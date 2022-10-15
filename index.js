1304d9fbe0bddbdf7fa38069f8b5f515b43eebef
anderdietrich <114541819+anderdietrich@users.noreply.github.com>
2022-10-14 21:34:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="index.js"></script>
-</head>
-<body>
-    <div class="center-fixed">
-        <h1>Cadastro de produto</h1>
-        <form id="frmCadastro">
-            <section>
-                <label for="txtCod">Código:</label><br>
-                <input id="txtCod" type="text"><br>
-                <label for="txtNome">Nome:</label><br>
-                <input id="txtNome" type="text"><br>
-                <label for="txtDescricao">Valor:</label><br>
-                <input id="txtDescricao" type="text"><br>
-                <input type="submit" id="btnCadastrar" value="Cadastrar"><br>
-            </section>
-        </form>
-    </div>
-    <table>
-        <caption>
-            <h2>Lista de produtos</h2>
-        </caption>
-        <thead>
-            <tr>
-                <th>Código</th>
-                <th>Nome</th>
-                <th>Descrição</th>
-            </tr>
-        </thead>
-        <tbody id="tbodyResultados">
-        </tbody>
-        <tfoot></tfoot>
+window.onload = function () {
+    montarLista();
+    document.getElementById('frmCadastro').addEventListener('submit',adicionarOuAlterar);
+}
 
-    </table>
-</body>
-</html>
+function adicionarOuAlterar(e) {
+    var txtCod = document.getElementById('txtCod');
+    var txtNome = document.getElementById('txtNome');
+    var txtDescricao = document.getElementById('txtDescricao');
+    var produto = {
+        codigo: txtCod.value,
+        nome: txtNome.value,
+        descricao: txtDescricao.value
+    };
+    var produtos = [];
+    var produtosLocalStorage = localStorage.getItem('produtos');
+    if (produtosLocalStorage != null){
+        produtos = JSON.parse(produtosLocalStorage)
+    };
+    produtos.push(produto);
+    localStorage.setItem('produtos', JSON.stringify(produtos));
+    montarLista();
+    document.getElementById('frmCadastro').requestFullscreen();
+    e.preventDefault(); 
+}
+
+function montarLista() {
+    var produtosLocalStorage = localStorage.getItem('produtos');
+    if (produtosLocalStorage == null) {
+        return;
+    };
+    var produtos = [];
+    produtos = JSON.parse(produtosLocalStorage);
+    var tbody = document.getElementById('tbodyResultados');
+    tbody.innerHTML = '';
+    produtos.map((produto, index) => {
+        tbody.innerHTML +=
+            '<tr id="rowTable' + index + '">' +
+                '<td>' + produto.codigo + '</td>' +
+                '<td>' + produto.nome + '</td>' +
+                '<td>' + produto.descricao + '</td>' +
+            '</tr>'
+
+    })
+}
