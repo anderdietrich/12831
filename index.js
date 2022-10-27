f44ac7a9beac4c8690f812e67f1a71597c02c037
anderdietrich <anderdietrich.cwb@gmail.com>
2022-10-27 20:27:37 -0300

    swagger

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.static('public'));
+
+/* #swagger.parameters['parameterName'] = {
+        in: <string>,
+        description: <string>,
+        required: <boolean>,
+        type: <string>,
+        format: <string>,
+        schema: <array>, <object> or <string>
+} */
 app.get('/', (req, res) => {
+  // #swagger.tags = ['Users']
+  // #swagger.summary = 'Some summary...'
+  // #swagger.description = 'Some description...'
+  // #swagger.operationId = 'Your_operationId_here'
   res.send('Hello World!')
 })
 
@@ -10,16 +27,6 @@ app.post('/', function (req, res) {
   res.send('Got a POST request');
 });
 
-app.post('/', function (req, res) {
-  res.send('Got a POST request');
-});
-
-app.post('/', function (req, res) {
-  res.send('Got a POST request');
-});
-
-app.use(express.static('public'));
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
